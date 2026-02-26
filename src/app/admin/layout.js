f1a1ac3654abba091fc2ebbb8919b5a819ec9dea
kerala govt employees <saheerk907@gmail.com>
2026-02-26 14:53:17 +0530

    Update layout.js

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,12 +1,3 @@
-import { supabase } from "@/lib/supabase";
-import { redirect } from "next/navigation";
-
-export default async function AdminLayout({ children }) {
-  const { data } = await supabase.auth.getUser();
-
-  if (!data?.user) {
-    redirect("/admin/login");
-  }
-
+export default function AdminLayout({ children }) {
   return <>{children}</>;
 }
